0771d9926c0bbf1c930db96da76aefe65c9ef2b4
Joan Romero <romerojoan1999@gmail.com>
2023-02-07 15:02:27 -0400

    add client origins to cors

--- src/utils/allowedOrigins.ts
+++ src/utils/allowedOrigins.ts
@@ -1,3 +1,7 @@
-const allowedOrigins: string[] = ['http://localhost:5173'];
+const allowedOrigins: string[] = [
+	'https://sticky-notes-client-vue.vercel.app',
+	'https://sticky-notes-client-1n2tc28ne-romerojoan1999-gmailcom.vercel.app',
+	'https://sticky-notes-client-vue-git-main-romerojoan1999-gmailcom.vercel.app',
+];
 
 export default allowedOrigins;
